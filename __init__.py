5abebe91b7fbada27f3c0dc5aafc400846334712
Santiago Bacaro <sbacarob@outlook.com>
2016-10-09 20:34:21 -0500

    Integración de gráficas. Se añadió algo de data binding de polymer. Se carga información inicial de servicios montados en Heroku

--- __init__.py
+++ __init__.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from requests import get
 
 app= Flask(__name__)
+
+app.jinja_env.globals.update(get=get)
+
+cl3= get('http://mt-ds4.herokuapp.com').content
+app.jinja_env.globals.update(cl3=cl3)
 
 @app.route('/')
 def homepage():
