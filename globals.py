bf0e2f93ab1fb173065bac339adc59f4d0ec67da
Santiago Bacaro <sbacarob@outlook.com>
2016-10-10 01:09:40 -0500

    Se movió la actualización minuto a minuto al lado del cliente

--- globals.py
+++ globals.py
@@ -1,11 +1,10 @@
 from requests import get
 
-cl1= get('http://mt-ds1.herokuapp.com').content
-rdm= get('http://mt-ds2.herokuapp.com').content
-cl2= get('http://mt-ds3.herokuapp.com').content
-cl3= get('http://mt-ds4.herokuapp.com').content
-
 def set_globals(app):
+	cl1= get('http://mt-ds1.herokuapp.com').content
+	rdm= get('http://mt-ds2.herokuapp.com').content
+	cl2= get('http://mt-ds3.herokuapp.com').content
+	cl3= get('http://mt-ds4.herokuapp.com').content
 	app.jinja_env.globals.update(cl1=cl1)
 	app.jinja_env.globals.update(rdm=rdm)
 	app.jinja_env.globals.update(cl2=cl2)
--- static/js/chartm.js
+++ static/js/chartm.js
@@ -1,48 +1,69 @@
 $('Document').ready(function(){
-				chart= new CanvasJS.Chart("grafica1",{
-					title:{
-						text:"Clima en Bogotá",
-						fontColor:"#7986cb"
-					},
-					data:[
-					{
-						type:"spline",
-						dataPoints: []
-					}]
-				});
+	chart= new CanvasJS.Chart("grafica1",{
+		title:{
+			text:"Clima en Bogotá",
+			fontColor:"#7986cb"
+		},
+		data:[
+		{
+			type:"spline",
+			dataPoints: []
+		}]
+	});
+		chart2= new CanvasJS.Chart("grafica2",{
+		title:{
+			text:"Números aleatorios",
+			fontColor:"#7986cb"
+		},
+		data:[{
+			type:"spline",
+			dataPoints:[]
+		}]
+	});
+		chart3= new CanvasJS.Chart("grafica3",{
+		title:{
+			text:"Clima en Manizales",
+			fontColor:"#7986cb"
+		},
+		data:[
+		{
+			type:"spline",
+			dataPoints:[]
+		}]
+	});
+		chart4= new CanvasJS.Chart("grafica4",{
+		title:{
+			text:"Clima en Chigorodó",
+			fontColor:"#7986cb"
+		},
+		data:[
+		{
+			type:"spline",
+			dataPoints:[]
+		}]
+	});	
+});
 
-				chart2= new CanvasJS.Chart("grafica2",{
-					title:{
-						text:"Números aleatorios",
-						fontColor:"#7986cb"
-					},
-					data:[{
-						type:"spline",
-						dataPoints:[]
-					}]
-				});
-
-				chart3= new CanvasJS.Chart("grafica3",{
-					title:{
-						text:"Clima en Manizales",
-						fontColor:"#7986cb"
-					},
-					data:[
-					{
-						type:"spline",
-						dataPoints:[]
-					}]
-				});
-
-				chart4= new CanvasJS.Chart("grafica4",{
-					title:{
-						text:"Clima en Chigorodó",
-						fontColor:"#7986cb"
-					},
-					data:[
-					{
-						type:"spline",
-						dataPoints:[]
-					}]
-				});
-			});
+function updateData(){
+	var dt= new Date()
+	$.get('http://mt-ds1.herokuapp.com',function(data){
+		cl1=parseFloat(data);
+		chart.options.data[0].dataPoints.push({x:dt,y:cl1});
+		chart.render();
+	});
+	$.get('http://mt-ds2.herokuapp.com',function(data){
+		rdm=parseInt(data);
+		chart2.options.data[0].dataPoints.push({x:dt,y:rdm});
+		chart2.render();
+	});
+	$.get('http://mt-ds3.herokuapp.com',function(data){
+		cl2=parseFloat(data);
+		chart3.options.data[0].dataPoints.push({x:dt,y:cl2});
+		chart3.render();
+	});
+	$.get('http://mt-ds4.herokuapp.com',function(data){
+		cl3=parseFloat(data);
+		chart4.options.data[0].dataPoints.push({x:dt,y:cl3});
+		chart4.render();
+	});
+}
